d67a3e4773a3b62fe30c6c0c02bb48b6ebd44dbb
Vojtanooo <janecek.vojta@gmail.com>
2024-02-02 12:42:34 +0100

    created dynamic loading data from database

--- demo/test_app/urls.py
+++ demo/test_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .random_number import func_random_number
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('get_random_number/', func_random_number, name='get_random_number')
+    path('get_random_number/', views.func_random_number, name='get_random_number'),
+    path('get_data_from_database/', views.get_data_from_database, name='get_data_from_database'),
 ]
--- demo/test_app/views.py
+++ demo/test_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, HttpResponse
-from .random_number import func_random_number
+from .models import Get_battery_Data
+from django.http import JsonResponse
+import random
 
-# Create your views here.
-def home(request):
-    return render(request, "home.html", {"number": func_random_number}) 
+def func_random_number(request):
+    number = random.randint(1, 100)
+    return JsonResponse({'number': number})
+
+def get_data_from_database(request):
+    soc = Get_battery_Data.objects.all().order_by('-time_stamp')[0].state_of_charge
+    actual_power = Get_battery_Data.objects.all().order_by('-time_stamp')[0].actual_power
+    return JsonResponse({"soc": soc, "actual_power" : actual_power})
+                         
+def home(request): 
+    return render(request, "home.html") 
