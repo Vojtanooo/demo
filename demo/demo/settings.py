96fd93144ae58e55e0805c124cd7d63d4aaeced4
Vojtanooo <janecek.vojta@gmail.com>
2024-02-02 11:38:54 +0100

    setup datatabse connection

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from . import pwd
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,9 +77,16 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'django_demo_database',  
+        'USER': 'root',  
+        'PASSWORD': pwd._PWD,  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }
+    } 
 }
 
 
